2926094fa4af662f4e9b7fa02f360978756e661f
Mikhail Koryukov <mezhovkin@yandex.ru>
2022-05-02 16:20:54 +0300

    initial commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+export const BACK_URL = 'http://localhost:4000';
+
+export const headers = {
+  Accept: 'application/json',
+  'Content-Type': 'application/json',
+};
+
+export const fixPromise = (res) => (
+  res.ok ? res.json()
+    : Promise.reject(`Произошла ошибка ${res.status}: ${res.statusText}`)
+);
+
+export const payments = (CardNumber, ExpDate, Cvv, Amount) => fetch(`${BACK_URL}/payments`, {
+  method: 'POST',
+  headers,
+  body: JSON.stringify({
+    CardNumber, ExpDate, Cvv, Amount
+  }),
+}).then((res) => fixPromise(res));
